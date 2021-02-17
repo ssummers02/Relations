040b465ac54de58056bbc0996c61dc769016c8ad
Ssummers <Ololosha102>
2021-02-17 20:25:15 +0300

    add

---
+++ Relations_project/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+
+namespace Relations_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grafPictureBox = new System.Windows.Forms.PictureBox();
+            this.listXComboBox = new System.Windows.Forms.ComboBox();
+            this.listYComboBox = new System.Windows.Forms.ComboBox();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.grafPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grafPictureBox
+            // 
+            this.grafPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.grafPictureBox.Location = new System.Drawing.Point(400, 12);
+            this.grafPictureBox.Name = "grafPictureBox";
+            this.grafPictureBox.Size = new System.Drawing.Size(400, 400);
+            this.grafPictureBox.TabIndex = 1;
+            this.grafPictureBox.TabStop = false;
+            // 
+            // listXComboBox
+            // 
+            this.listXComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.listXComboBox.FormattingEnabled = true;
+            this.listXComboBox.Items.AddRange(new object[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            this.listXComboBox.Location = new System.Drawing.Point(50, 20);
+            this.listXComboBox.Name = "listXComboBox";
+            this.listXComboBox.Size = new System.Drawing.Size(40, 20);
+            this.listXComboBox.TabIndex = 6;
+            this.listXComboBox.SelectedIndexChanged += ListXComboBoxOnSelectedIndexChanged;
+            // 
+            // listYComboBox
+            // 
+            this.listYComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.listYComboBox.FormattingEnabled = true;
+            this.listYComboBox.Items.AddRange(new object[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            this.listYComboBox.Location = new System.Drawing.Point(50, 20+35);
+            this.listYComboBox.Name = "listYComboBox";
+            this.listYComboBox.Size = new System.Drawing.Size(40, 20);
+            this.listYComboBox.TabIndex = 6;
+            this.listYComboBox.SelectedIndexChanged += ListYComboBoxOnSelectedIndexChanged;
+            
+            // 
+            // xLabel
+            // 
+            this.xLabel.Location = new System.Drawing.Point(20, 20);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(30, 20);
+            this.xLabel.TabIndex = 11;
+            this.xLabel.Text = "X";
+            this.xLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yLabel
+            // 
+            this.yLabel.Location = new System.Drawing.Point(20, 20+35);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(30, 20);
+            this.yLabel.TabIndex = 12;
+            this.yLabel.Text = "Y";
+            this.yLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Relations_project.Properties.Resources.color_2174049_1280;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.listXComboBox);
+            this.Controls.Add(this.listYComboBox);
+            this.Controls.Add(this.grafPictureBox);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Графы";
+            ((System.ComponentModel.ISupportInitialize) (this.grafPictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.ComboBox listYComboBox;
+        private System.Windows.Forms.ComboBox listXComboBox;
+        private System.Windows.Forms.PictureBox grafPictureBox;
+        #endregion
+    }
+}
---
+++ Relations_project/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Relations_project
+{
+    public partial class Form1 : Form
+    {
+        private int countGraf = 10;
+        private int check;
+        private List<int> xList = new List<int>();
+        private List<int> yList = new List<int>();
+
+        List<List<int>> list = new List<List<int>>();
+        List<TextBox> xListTextBoxes = new List<TextBox>();
+        List<TextBox> yListTextBoxes = new List<TextBox>();
+        int countX = 1;
+        int countY = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listXComboBox.SelectedIndex = 0;
+            listYComboBox.SelectedIndex = 0;
+        }
+
+
+        private void PaintLine(Graphics graphics, Pen pen, PointF[] points)
+        {
+            for (int i = 0; i < countGraf; i++)
+            {
+                points[i].X += 15;
+                points[i].Y += 15;
+            }
+
+            for (int i = 0; i < countGraf; i++)
+            {
+                for (int j = 0; j < countGraf; j++)
+                {
+                    if (list[i][j] > 0)
+                        graphics.DrawLine(pen, points[i], points[j]);
+                }
+            }
+        }
+
+        private void PictureBoxPaint()
+        {
+            grafPictureBox.Refresh();
+            double alfa = 2 * Math.PI / countGraf;
+            PointF[] points = new PointF[countGraf];
+            Graphics graphics = grafPictureBox.CreateGraphics();
+            Brush brush = new SolidBrush(Color.Blue);
+            Pen pen = new Pen(Color.Blue, 3F);
+
+            for (int i = 0; i < countGraf; i++)
+            {
+                int x = Convert.ToInt32(200 + 150 * Math.Cos(alfa * i));
+                int y = Convert.ToInt32(200 + 150 * Math.Sin(alfa * i));
+                if (countGraf == 1)
+                    x = y = 200;
+                points[i] = new PointF(x, y);
+                graphics.DrawEllipse(pen, points[i].X, points[i].Y, 30, 30);
+                graphics.FillEllipse(brush, new Rectangle((int) points[i].X, (int) points[i].Y, 30, 30));
+            }
+
+
+            PaintLine(graphics, pen, points);
+            for (int i = 0; i < countGraf; i++)
+                PaintText(points, graphics, i);
+        }
+
+        private void KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+                e.Handled = true;
+        }
+
+        private void creatGrafButton_Click(object sender, EventArgs e)
+        {
+            if (grafPictureBox.Image != null)
+            {
+                grafPictureBox.Image.Dispose();
+                grafPictureBox.Image = null;
+            }
+
+            grafPictureBox.Refresh();
+        }
+        
+
+        /*
+        private void PrintsubgraphsCountLabel(Graph g)
+        {
+            subgraphsCountLabel = new Label();
+            subgraphsCountLabel.AutoSize = true;
+            subgraphsCountLabel.BackColor = Color.Transparent;
+            subgraphsCountLabel.Font = new Font("Times New Roman", 14F, FontStyle.Bold);
+            subgraphsCountLabel.ForeColor = Color.Gray;
+            subgraphsCountLabel.Location = new Point(20, 20 + 35 * (countGraf + 3));
+            subgraphsCountLabel.Name = "subgraphsCountLabel";
+            subgraphsCountLabel.Size = new Size(200, 19);
+            subgraphsCountLabel.Text = $@"Количество подграфов: {g.SubgraphsCount()}";
+            this.Controls.Add(subgraphsCountLabel);
+        }
+        */
+
+
+        private void PaintText(PointF[] points, Graphics graphics, int i)
+        {
+            var f = new Font(Font.FontFamily, 15);
+            string text = Convert.ToString(i + 1);
+            var size = graphics.MeasureString(text, f);
+            var pt = new PointF(points[i].X + 7 - 15, points[i].Y + 3 - 15);
+            var rect = new RectangleF(pt, size);
+            graphics.DrawString(text, f, Brushes.FloralWhite, rect);
+        }
+
+
+        private void ListYComboBoxOnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            int x = 30 + 35 * 2;
+            int y = 20 + 35;
+
+            for (int i = 0; i < yListTextBoxes.Count; i++)
+            {
+                Controls.Remove(yListTextBoxes[i]);
+            }
+
+            yListTextBoxes.Clear();
+            countY = Convert.ToInt32(listYComboBox.SelectedItem.ToString());
+
+            for (int i = 0; i < countY; i++)
+            {
+                TextBox tempTextBox = new TextBox()
+                {
+                    Name = Convert.ToString(i),
+                    Location = new Point(x + 30 * i, y),
+                    Size = new System.Drawing.Size(25, 20),
+                    TextAlign = HorizontalAlignment.Center,
+                    Text = "0"
+                };
+                tempTextBox.KeyPress += KeyPress;
+
+                yListTextBoxes.Add(tempTextBox);
+                Controls.Add(yListTextBoxes[i]);
+            }
+        }
+
+        private void ListXComboBoxOnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            int x = 30 + 35 * 2;
+            int y = 20;
+
+            for (int i = 0; i < xListTextBoxes.Count; i++)
+            {
+                Controls.Remove(xListTextBoxes[i]);
+            }
+
+            xListTextBoxes.Clear();
+
+            countX = Convert.ToInt32(listXComboBox.SelectedItem.ToString());
+
+            for (int i = 0; i < countX; i++)
+            {
+                TextBox tempTextBox = new TextBox()
+                {
+                    Name = Convert.ToString(i),
+                    Location = new Point(x + 30 * i, y),
+                    Size = new System.Drawing.Size(25, 20),
+                    TextAlign = HorizontalAlignment.Center,
+                    Text = "0",
+                };
+                tempTextBox.KeyPress += KeyPress;
+                xListTextBoxes.Add(tempTextBox);
+                Controls.Add(xListTextBoxes[i]);
+            }
+        }
+    }
+}
